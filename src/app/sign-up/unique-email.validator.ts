5cff9c9e2687d340d99a063e1cced295c20e752e
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-12-05 13:27:54 +0100

    add unique-email.validator.ts for connect with backend and correct email is use. Add test

---
+++ src/app/sign-up/unique-email.validator.ts
@@ -0,0 +1,26 @@
+import {Injectable} from "@angular/core";
+import {AbstractControl, AsyncValidator, ValidationErrors} from "@angular/forms";
+import {UserService} from "../core/user.service";
+import { catchError, map, Observable, of } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UniqueEmailValidator implements AsyncValidator {
+
+  constructor(private userService: UserService) {
+  }
+
+  validate(control: AbstractControl): Promise<ValidationErrors | null>
+    | Observable<ValidationErrors | null> {
+
+    return this.userService.isEmailTaken(control.value).pipe(
+      map((_) => (_ ? { uniqueEmail: true} : null)),
+      catchError(() => of(null))
+    )
+  }
+
+
+
+}
