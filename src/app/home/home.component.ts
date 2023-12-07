68b1ab3ddaa4e4bfd48cae09fe2c2d9cede0b758
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-12-07 13:37:00 +0100

    add example how observable works in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {first, map, Observable, Subscriber} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  subscriber?: Subscriber<string>;
+
+  observable = new Observable<string>((sub) => {
+    this.subscriber = sub;
+  });
+
+  message = 'No message';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  subscribe() {
+    this.observable.pipe(map(value => '---' + value + '---'))
+      .subscribe(data => this.message = data);
+
+    // this.observable.subscribe({
+    //   next: (data) => {
+    //     this.message = data;
+    //   },
+    //   complete: () => {
+    //     this.message = 'Completed';
+    //   },
+    //   error: (err) => {
+    //     this.message = err.message;
+    //   }
+    // });
+  }
+
+  publish() {
+    this.subscriber?.next('' + Math.random() * 100);
+  }
+
+  complete() {
+    this.subscriber?.complete();
+  }
+
+  error() {
+    this.subscriber?.error(new Error('dummy error'));
   }
 
 }
