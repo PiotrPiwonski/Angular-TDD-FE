b149f06764e0696b3fa8b7f324a8b4c6b4a42fd4
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-12-08 13:49:16 +0100

    add user-list component, add types and loadUsers function in user service and add first test in user-list

---
+++ src/app/home/user-list/user-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../core/user.service";
+import {UserPage} from "../../shared/types";
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  page: UserPage = {
+    content: [],
+    page: 0,
+    size: 3,
+    totalPages: 0
+  }
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.loadUsers().subscribe(responseBody => {
+      this.page = responseBody as UserPage;
+    });
+  }
+
+}
